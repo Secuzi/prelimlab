6c7e7b6bb7af46a3ddd1fa7932e4bd3aff322171
Harold <secuzi@gmail.com>
2024-08-18 16:19:20 +0800

    Completed the flyout

--- prelimlab/Views/HomePage.xaml.cs
+++ prelimlab/Views/HomePage.xaml.cs
@@ -19,11 +19,7 @@ public partial class HomePage : FlyoutPage
 	void LoadCuties()
 	{
 		var cuties = new ObservableCollection<CutieModel>(CutieModelRepository.GetCuties());
-		HomePageImg.Source = cuties[0].Image.Source;
-		HomePageImg.Aspect = cuties[0].Image.Aspect;
-		lbltitle.Text = cuties[0].Title;
-		lblUser.Text += $"{cuties[0].User}";
-		lblDescription.Text = cuties[0].Description;
+		
 		
 
 	}
