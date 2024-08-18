49bb916e399671a2861fac14c5f83e5ffd5c4de7
Harold <secuzi@gmail.com>
2024-08-18 08:14:28 +0800

    Added more properties for the model

--- prelimlab/Models/CutieModel.cs
+++ prelimlab/Models/CutieModel.cs
@@ -8,9 +8,10 @@ namespace prelimlab.Models
 {
     class CutieModel
     {
+        public int CutieId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-
+        public string Image { get; set; }
         public string User { get; set; }
 
     }
